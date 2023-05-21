0117f071881619655191a68b46c25af8d6024c46
Zhiwei Yang <96382703+2665477495@users.noreply.github.com>
2023-05-21 19:39:23 +0800

    Update decentralized_lottery.sol
    add NatSpec comments

--- decentralized_lottery.sol
+++ decentralized_lottery.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/// @title Decentralized Lottery Contract
+/// @dev Contract to manage a decentralized lottery system
 contract DecentralizedLottery {
     struct Participant {
         bytes32 commitment;
@@ -18,34 +20,44 @@ contract DecentralizedLottery {
     event Reveal(address indexed user, uint randomNumber);
     event Winner(address indexed user, uint reward);
 
+    /// @dev Constructor sets the casino owner of the contract
     constructor() {
         casino = msg.sender;
     }
 
+    /// @dev Ensures that only the casino can call the function
     modifier onlyCasino() {
         require(msg.sender == casino, "Only the casino can call this function.");
         _;
     }
 
+    /// @dev Ensures that the function can be called only after the submission period ends
     modifier onlyAfterSubmissionEnd() {
         require(block.timestamp >= submissionEndTime, "Submission period has not ended yet.");
         _;
     }
 
+    /// @dev Ensures that the function can be called only before the reveal period ends
     modifier onlyBeforeRevealEnd() {
         require(block.timestamp < revealEndTime, "Reveal period has ended.");
         _;
     }
 
+    /// @notice Set the reward for the lottery
+    /// @param amount The reward amount
     function setReward(uint amount) external onlyCasino {
         reward = amount;
     }
 
+    /// @notice Start the submission period for the lottery
+    /// @param submissionDuration Duration of the submission period in seconds
     function startSubmission(uint submissionDuration) external onlyCasino {
         require(submissionEndTime == 0, "Submission period has already started.");
         submissionEndTime = block.timestamp + submissionDuration;
     }
 
+    /// @notice Submit a commitment to participate in the lottery
+    /// @param commitment The hashed commitment of the participant
     function submitHash(bytes32 commitment) external payable {
         require(block.timestamp < submissionEndTime, "Submission period has ended.");
         require(msg.value >= reward, "Insufficient value sent.");
@@ -55,11 +67,15 @@ contract DecentralizedLottery {
         emit Submission(msg.sender);
     }
 
+    /// @notice Start the reveal period for the lottery
+    /// @param revealDuration Duration of the reveal period in seconds
     function startReveal(uint revealDuration) external onlyCasino onlyAfterSubmissionEnd {
         require(revealEndTime == 0, "Reveal period has already started.");
         revealEndTime = submissionEndTime + revealDuration;
     }
 
+    /// @notice Reveal the random number for the lottery
+    /// @param randomNumber The random number of the participant
     function reveal(uint randomNumber) external onlyAfterSubmissionEnd onlyBeforeRevealEnd {
         bytes32 commitment = keccak256(abi.encodePacked(randomNumber, msg.sender));
         
@@ -69,6 +85,7 @@ contract DecentralizedLottery {
         emit Reveal(msg.sender, randomNumber);
     }
 
+    /// @notice Determine the winner of the lottery
     function determineWinner() external onlyCasino onlyAfterSubmissionEnd {
         require(block.timestamp >= revealEndTime, "Reveal period has not ended yet.");
 
@@ -86,6 +103,7 @@ contract DecentralizedLottery {
         resetLottery();
     }
 
+    /// @dev Reset the lottery game
     function resetLottery() private {
         reward = 0;
         submissionEndTime = 0;
